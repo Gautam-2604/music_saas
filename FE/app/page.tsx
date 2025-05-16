9a4f1783a39a1555e617e2a5dbfb2bc22d7752ff
Gautam-2604 <gaurisariag@gmail.com>
2025-05-17 00:55:57 +0530

    some changes

--- FE/app/page.tsx
+++ FE/app/page.tsx
@@ -1,9 +1,17 @@
+'use client';
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hi
+    <div className="flex justify-between">
+      <div>
+        Muzii
+      </div>
+      <div>
+        <button className="m-2 p-2 bg-blue-400" onClick={()=>signIn()}>Sign In</button>
+      </div>
+
     </div>
   );
 }
